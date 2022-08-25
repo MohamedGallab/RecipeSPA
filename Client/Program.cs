daa45783e1f2bb862a57815c7643324877fcc7cb
Mohamed Gallab <mohamed.w.gallab@gmail.com>
2022-08-25 03:37:23 +0300

    anti forgery now works

--- Client/Program.cs
+++ Client/Program.cs
@@ -5,20 +5,11 @@ builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-	app.UseExceptionHandler("/Error");
-	app.UseHsts();
-}
-
 app.UseHttpsRedirection();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
 app.UseRouting();
-
-app.UseAuthorization();
 
 app.MapRazorPages();
 
